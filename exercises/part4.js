c5d6c7744528c5bffe00c844ca6a44f37f267b46
Melva Lang <mjlang74@gmail.com>
2019-11-12 03:22:04 -0600

    clean up part4.js

--- exercises/part4.js
+++ exercises/part4.js
@@ -1,15 +1,6 @@
 var kilometersToMars = 225000000;
 var kilometersToTheMoon = 384400;
-// The variables that are commented out will be moved into the Spacecraft class
-// let spaceCraft: string = "Space Shuttle";
-// let speedMph: number = 17500;
-// let milesPerKilometer: number = 0.621;
-// This function will also be moved into the Spacecraft class
-// function getDaysToLocation(kilometersAway: number): number {
-//   let milesAway: number = kilometersAway * milesPerKilometer;
-//   let hours: number = milesAway / speedMph;
-//   return hours / 24;
-// }
+
 // Define your Spacecraft class here:
 var Spacecraft = /** @class */ (function () {
     function Spacecraft(name, speedMph) {
