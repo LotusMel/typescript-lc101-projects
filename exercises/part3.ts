d30a5a0c3798d092aece1318f509634accc8ed43
Melva Lang <mjlang74@gmail.com>
2019-11-11 23:04:32 -0600

    part 3

--- exercises/part3.ts
+++ exercises/part3.ts
@@ -1,17 +1,23 @@
 // URL for the instructions: 
 // https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
 
-spacecraftName = "Space Shuttle";
-speedMph = 17500;
-kilometersToMars = 225000000;
-kilometersToTheMoon = 384400;
-milesPerKilometer = 0.621;
+let spacecraftName: string = "Space Shuttle";
+let speedMph: number = 17500;
+let kilometersToMars: number = 225000000;
+let kilometersToTheMoon: number = 384400;
+let milesPerKilometer: number = 0.621;
 
 // Code the "getDaysToLocation" function here:
 
-
-
-
-
+function getDaysToLocation(kilometersAway: number): number {
+    let milesAway: number = kilometersAway * milesPerKilometer;
+    let hours: number = milesAway / speedMph;
+    let days: number = hours / 24;
+    
+    return days;
+}
 
 // Call the function and print the outputs for the Mars trip and the moon trip:
+
+console.log(`${spacecraftName} would take ${getDaysToLocation(kilometersToMars)} days to get to Mars.`);
+console.log(`${spacecraftName} would take ${getDaysToLocation(kilometersToTheMoon)} days to get to the Moon.`);
