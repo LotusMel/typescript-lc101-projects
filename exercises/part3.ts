7628ab979f12413cdf6b971ee19ebac87f8239aa
Melva Lang <mjlang74@gmail.com>
2019-11-12 02:49:22 -0600

    part4

--- exercises/part3.ts
+++ exercises/part3.ts
@@ -9,13 +9,6 @@ let milesPerKilometer: number = 0.621;
 
 // Code the "getDaysToLocation" function here:
 
-function getDaysToLocation(kilometersAway: number): number {
-    let milesAway: number = kilometersAway * milesPerKilometer;
-    let hours: number = milesAway / speedMph;
-    let days: number = hours / 24;
-    
-    return days;
-}
 
 // Call the function and print the outputs for the Mars trip and the moon trip:
 
--- exercises/part4.ts
+++ exercises/part4.ts
@@ -15,11 +15,31 @@
 // }
 
 // Define your Spacecraft class here:
-
-
+class Spacecraft {
+    milesPerKilometer: number = 0.621;
+    name: string;
+    speedMph: number;
+    constructor(name: string, speedMph: number) {
+        this.name = name;
+        this.speedMph = speedMph;
+    }
+    getDaysToLocation(kilometersAway: number): number {
+        let milesAway: number = kilometersAway * this.milesPerKilometer;
+        let hours: number = milesAway / this.speedMph;
+        let days: number = hours / 24;
+        
+        return days;
+    }
+    
+}
 
 // Create an instance of the class here:
 
+let spaceShuttle = new Spacecraft("Determination", 17500);
+
 
 // Print two outputs - one for the trip to Mars and one for the trip to the moon.
+
+console.log(`${spaceShuttle.name} would take ${spaceShuttle.getDaysToLocation(kilometersToMars)} to get to Mars.`);
+console.log(`${spaceShuttle.name} would take ${spaceShuttle.getDaysToLocation(kilometersToTheMoon)} to get to the Moon.`);
 
