800c68997dfec54d743044938981bf4a8e9bd483
Melva Lang <mjlang74@gmail.com>
2019-11-12 03:12:49 -0600

    part4 changes for readability

--- exercises/part4.ts
+++ exercises/part4.ts
@@ -1,19 +1,6 @@
 let kilometersToMars: number = 225000000;
 let kilometersToTheMoon: number = 384400;
 
-
-// The variables that are commented out will be moved into the Spacecraft class
-// let spaceCraft: string = "Space Shuttle";
-// let speedMph: number = 17500;
-// let milesPerKilometer: number = 0.621;
-
-// This function will also be moved into the Spacecraft class
-// function getDaysToLocation(kilometersAway: number): number {
-//   let milesAway: number = kilometersAway * milesPerKilometer;
-//   let hours: number = milesAway / speedMph;
-//   return hours / 24;
-// }
-
 // Define your Spacecraft class here:
 class Spacecraft {
     milesPerKilometer: number = 0.621;
@@ -30,16 +17,12 @@ class Spacecraft {
         
         return days;
     }
-    
 }
 
 // Create an instance of the class here:
-
 let spaceShuttle = new Spacecraft("Determination", 17500);
 
-
 // Print two outputs - one for the trip to Mars and one for the trip to the moon.
-
 console.log(`${spaceShuttle.name} would take ${spaceShuttle.getDaysToLocation(kilometersToMars)} to get to Mars.`);
 console.log(`${spaceShuttle.name} would take ${spaceShuttle.getDaysToLocation(kilometersToTheMoon)} to get to the Moon.`);
 
