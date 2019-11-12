0941bd8857b5dd7adaba203841aef706be433ebb
Melva Lang <mjlang74@gmail.com>
2019-11-11 22:20:01 -0600

    parts 1 and 2

--- exercises/part3.ts
+++ exercises/part3.ts
@@ -1,13 +1,14 @@
 // URL for the instructions: 
 // https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
 
-let spacecraftName: string = "Space Shuttle";
-let speedMph: number = 17500;
-let kilometersToMars: number = 225000000;
-let kilometersToTheMoon: number = 384400;
-let milesPerKilometer: number = 0.621;
+spacecraftName = "Space Shuttle";
+speedMph = 17500;
+kilometersToMars = 225000000;
+kilometersToTheMoon = 384400;
+milesPerKilometer = 0.621;
 
 // Code the "getDaysToLocation" function here:
+
 
 
 
