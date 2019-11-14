038fbc673608b41d56f05616c1d3f6382a7b6f96
Melva Lang <mjlang74@gmail.com>
2019-11-13 19:48:15 -0600

    still working

---
+++ studio/Cargo.ts
@@ -0,0 +1,11 @@
+import { Payload } from './Payload';
+
+export class Cargo implements Payload {
+    massKg: number;
+    material: string;
+
+    constructor(massKg: number, name: string) {
+        this.massKg = massKg;
+        this.material = this.material;
+    }
+}
---
+++ studio/Rocket.ts
@@ -0,0 +1,36 @@
+import { Payload } from './Payload';
+import { Cargo } from './Cargo';
+import { Astronaut } from './Astronaut';
+
+export class Rocket implements Payload {
+    massKg: number;
+    totalCapacityKg: number;
+    name: string;
+    cargoItems: Cargo[];
+    astronauts: Astronaut[];
+
+    constructor(totalCapacityKg: number, name: string) {
+        this.totalCapacityKg = totalCapacityKg;
+        this.name = name;
+    }
+
+    sumMass( items: Payload[] ): number {
+       
+        let total: number = 0;
+
+        for(let i=0; i < items.length; i++){
+            total += items[i].massKg;
+        }
+
+        return total;
+    }
+
+    currentMassKg(): number{
+        // Uses this.sumMass to return the combined mass of this.astronauts and this.cargoItems
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+    }
+
+
+
+
+}
